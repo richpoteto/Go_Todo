e11d454f505edee97819c860d9c650250e0c259b
richpoteto <richjiang.soft@gmail.com>
2019-08-18 22:13:34 -0400

    add edit-label

---
+++ public/app/components/label/edit-label/edit-label.component.js
@@ -0,0 +1,43 @@
+angular.module('editLabel').component('editLabel', {
+   templateUrl: 'components/label/edit-label/edit-label.tmpl.html',
+    scope: {},
+    controller: function($scope, $location, $routeParams, toaster, utils, labelResource) {
+        $scope.editing = utils.isAnExistingOne();
+        $scope.headerText = $scope.editing ? "Edit label" : "Create label";
+        $scope.label = {};
+        if ($scope.editing) {
+            $scope.save = function () {
+                labelResource.update({labelId: $routeParams.labelId}, $scope.label).$promise.then(function () {
+                    toaster.pop('info', 'Success', 'Label saved');
+                    $location.path('/labels');
+                }, function (resp) {
+                    toaster.pop('error', resp.data.title, resp.data.description);
+                    console.error('response', JSON.stringify(resp));
+                    $location.path('/labels');
+                });
+            };
+            labelResource.get({labelId: $routeParams.labelId}).$promise.then(function (label) {
+                for (let property in label) {
+                    if (label.hasOwnProperty(property)) {
+                        $scope.label[property] = label[property];
+                    }
+                }
+                toaster.pop('info', 'Success', 'Label loaded');
+            }, function (response) {
+                toaster.pop('error', response.data.title, response.data.description);
+                console.error('response', JSON.stringify(response));
+            });
+        } else {
+            $scope.save = function () {
+                labelResource.save($scope.label, function () {
+                    toaster.pop('info', 'Success', 'Label saved');
+                    $location.path('/labels');
+                }, function (resp) {
+                    toaster.pop('error', resp.data.title, resp.data.description);
+                    console.error('response', JSON.stringify(resp));
+                    $location.path('/labels');
+                });
+            };
+        }
+    }
+});
---
+++ public/app/components/label/edit-label/edit-label.module.js
@@ -0,0 +1,1 @@
+angular.module('editLabel', ['ngResource', 'toaster']);
